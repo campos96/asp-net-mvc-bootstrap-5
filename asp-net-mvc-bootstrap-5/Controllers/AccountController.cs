5c9e1a1bfa40e2c1fa20e4ba0cff4dfafe5cee96
cesarhc96 <cesarhcmartinez@gmail.com>
2022-08-13 18:26:12 -0700

    Add login view

---
+++ asp-net-mvc-bootstrap-5/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace asp_net_mvc_bootstrap_5.Controllers
+{
+	public class AccountController : Controller
+	{
+		public IActionResult Index() => RedirectToAction(nameof(Login));
+
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+	}
+}
